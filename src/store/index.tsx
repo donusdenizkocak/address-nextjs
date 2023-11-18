8ff00f3ffe1803873f1ccf9ac76b5043a47fc0d0
denizderK <denizderya8668@gmail.com>
2023-11-18 23:59:12 +0300

    link

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,15 +1,15 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 
 //** Reducers */
 import country from "@/store/apps/country"
 
-export const store =configureStore({
+export const store = configureStore({
     reducer: {
         country
     },
-    middleware:getDefaultMiddleware =>
-    getDefaultMiddleware({serializableCheck:false})
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({ serializableCheck: false })
 })
 
-export type AddDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
